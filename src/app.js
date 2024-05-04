86aa7e11a06d20ed4dc45ad3f3a6771488ec162d
扈冲 <huchong102874>
2024-05-04 12:18:07 +0800

    新增上传功能

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-const onerror = require('koa-onerror')
-const app = require('./routes/index')
-// error handler
-app.use(require('koa-static')(__dirname + '/public'))
-
+const onerror = require("koa-onerror");
+const app = require("./routes/index");
+const static = require("koa-static");
+app.use(static(__dirname + "/.." + "/static")); // __dirname是当前文件夹
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
 });
-module.exports = app
+module.exports = app;
--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,79 +1,77 @@
-const inspect = require('util').inspect
-const path = require('path')
-const os = require('os')
-const fs = require('fs')
-const Busboy = require('busboy')
-const UtilType = require('./type')
-const UtilDatetime = require('./datetime')
-
+const path = require("path");
+const fs = require("fs");
+const Busboy = require("busboy");
+const UtilType = require("./type");
+const UtilDatetime = require("./datetime");
 
 function mkdirsSync(dirname) {
   // console.log(dirname)
   if (fs.existsSync(dirname)) {
-    return true
+    return true;
   } else {
     if (mkdirsSync(path.dirname(dirname))) {
-      fs.mkdirSync(dirname)
-      return true
+      fs.mkdirSync(dirname);
+      return true;
     }
   }
 }
 
-function getSuffixName( fileName ) {
-  let nameList = fileName.split('.')
-  return nameList[nameList.length - 1]
+function getSuffixName(fileName) {
+  let nameList = fileName.split(".");
+  return nameList[nameList.length - 1];
 }
 
-function uploadPicture( ctx, options) {
-  let req = ctx.req
-  let res = ctx.res
-  let busboy = new Busboy({headers: req.headers})
+function uploadPicture(ctx, options) {
+  let req = ctx.req;
+  let res = ctx.res;
+  let busboy = Busboy({ headers: req.headers });
 
-  let pictureType = 'common'
-  if ( UtilType.isJSON( options ) && UtilType.isString( options.pictureType ) ) {
-    pictureType = options.pictureType
+  let pictureType = "common";
+  if (UtilType.isJSON(options) && UtilType.isString(options.pictureType)) {
+    pictureType = options.pictureType;
   }
 
   let picturePath = path.join(
-    __dirname, 
-    '/../../static/output/upload/', 
-    pictureType, 
-    UtilDatetime.parseStampToFormat(null, 'YYYY/MM/DD'))
+    __dirname,
+    "/../../static/",
+    pictureType,
+    UtilDatetime.parseStampToFormat(null, "YYYYMM/DD")
+  );
 
-  let mkdirResult = mkdirsSync( picturePath )
-  
+  let mkdirResult = mkdirsSync(picturePath);
 
   return new Promise((resolve, reject) => {
-    let result = { 
+    let result = {
       success: false,
-      code: '',
-      message: '',
+      code: "",
+      message: "",
       data: null,
-      filename:"",
-      uploadFilePath:"",
-    }
+      filename: "",
+      uploadFilePath: "",
+    };
 
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      console.log('File-file [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype)
-      
+    busboy.on("file", function (fieldname, file, fileData) {
+      let { filename, encoding, mimetype } = fileData;
+      let pictureName =
+        Math.random().toString(16).substring(2) + "." + getSuffixName(filename);
+      let _uploadFilePath = path.join(picturePath, pictureName);
 
-      let pictureName = Math.random().toString(16).substr(2) + '.' + getSuffixName(filename)
-      let _uploadFilePath = path.join( picturePath, pictureName )
-      
-      let saveTo = path.join(_uploadFilePath)
-      file.pipe(fs.createWriteStream(saveTo))
+      let saveTo = path.join(_uploadFilePath);
+      file.pipe(fs.createWriteStream(saveTo));
 
       // file.on('data', function(data) {
       //   console.log('File-data [' + fieldname + '] got ' + data.length + ' bytes')
       // })
 
-      file.on('end', function() {
+      file.on("end", function () {
+        let url = picturePath.split("static")[1].replace(/\\/g,'/');
         result.success = true;
         result.filename = filename;
         result.uploadFilePath = pictureName;
-        resolve(result)
-      })
-    })
+        result.picturePath = url;
+        resolve(result);
+      });
+    });
 
     // busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
     //   console.log('Field-field [' + fieldname + ']: value: ' + inspect(val))
@@ -82,22 +80,15 @@ function uploadPicture( ctx, options) {
     //   console.log('Done parsing form!')
     // })
 
-    busboy.on('error', function(err) {
-      console.log('File-error')
-      reject(result)
-    })
+    busboy.on("error", function (err) {
+      console.log("File-error");
+      reject(result);
+    });
 
-    req.pipe(busboy)
-  })
-    
-} 
-
-
-module.exports =  {
-  uploadPicture,
+    req.pipe(busboy);
+  });
 }
 
-
-
-
-
+module.exports = {
+  uploadPicture,
+};
